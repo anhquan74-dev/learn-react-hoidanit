b8d8d0a1fef2b642c1eecd6b879861c819dab067
anhquan74.dev <anhquan74.dev@gmail.com>
2022-08-31 01:48:45 +0700

    video 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+        <div>My first component {Math.random()}</div>
+    );
+  }
+}
+
+// const MyComponent = () => {
+//   return <div></div>;
+// };
+
+export default MyComponent;
