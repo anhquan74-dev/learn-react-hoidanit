6df3058afe8386e63aa7cacf6315d24f424a4070
anhquan74.dev <anhquan74.dev@gmail.com>
2022-08-31 02:37:04 +0700

    video 19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,8 +6,23 @@ class MyComponent extends React.Component {
         city: "Da Nang",
         age: 24,
     };
+
+    handleClick(e) {
+        console.log(e, e.target)
+        console.log(e.target.innerText)
+    }
+
+    handleMouseOver(e) {
+        console.log(e.pageX)
+    }
+
     render() {
-        return <div> My name is {this.state.name} and I'm from {this.state.city}</div>;
+        return (<div>
+            My name is {this.state.name} and I'm from {this.state.city}
+            <br />
+            <button onClick={this.handleClick}>Click me</button>
+            <button onMouseOver={this.handleMouseOver}>Hover me</button>
+        </div>);
     }
 }
 
