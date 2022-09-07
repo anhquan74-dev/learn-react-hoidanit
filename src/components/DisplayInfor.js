e30a010fdd1c7422b30a48813001322e12336c48
anhquan74.dev <anhquan74.dev@gmail.com>
2022-09-07 17:55:33 +0700

    useEffect

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,89 +1,42 @@
 import React, { useState } from "react";
 import "./DisplayInfor.scss";
 import logo from "../logo.svg";
+import { useEffect } from "react";
 
-// stateless vs stateful
-// class DisplayInfor extends React.Component {
-//     // constructor(props) {
-//     //     super(props);
-//     //     this.state = {
-//     //         isShowListUsers: true,
-//     //     };
-//     //     console.log('>> 1. call me constructor()')
-//     // }
-
-//     // componentDidMount() {
-//     //     console.log('>> call me componentDidMount()')
-//     //     setTimeout(() => { document.title = 'anhquan' }, 3000)
-//     // }
-
-//     // componentDidUpdate(prevProps, prevState, snapshot) {
-//     //     console.log('>> call me componentDidUpdate()', this.props, prevProps)
-//     //     if (this.props.listUsers !== prevProps.listUsers) {
-//     //         if (this.props.listUsers.length === 5) {
-//     //             alert('You got 5 users')
-//     //         }
-//     //     }
-
-//     // }
-
-//     // handleOnclickShowHideUsers = () => {
-//     //     this.setState({
-//     //         isShowListUsers: !this.state.isShowListUsers,
-//     //     });
-//     // };
-
-//     render() {
-//         //console.log('>> call me render()')
-
-//         // console.log(this.props);
-//         const { listUsers } = this.props;
-
-//         return (
-//             <div className="display-infor-container">
-//                 <img src={logo} alt="logo" />
-//                 <br />
-//                 List Users:
-//                 {/* <span onClick={this.handleOnclickShowHideUsers}>
-//                     {this.state.isShowListUsers === true
-//                         ? "Hide List Users"
-//                         : "Show List Users"}
-//                 </span> */}
-//                 {true && (
-//                     <>
-//                         {listUsers.map((user, index) => {
-//                             return (
-//                                 <>
-//                                     <div className="user-item">
-//                                         <div
-//                                             key={user.id}
-//                                             className={`user-item-content ${user.age > 18 ? "green" : "red"
-//                                                 }`}
-//                                         >
-//                                             <h4>My name is {user.name}</h4>
-//                                             <h4>I'm {user.age}</h4>
-//                                         </div>
-//                                         <span
-//                                             className="delete-btn"
-//                                             onClick={() => this.props.handleDeleteUser(user.id)}
-//                                         >
-//                                             X
-//                                         </span>
-//                                     </div>
-//                                     <hr />
-//                                 </>
-//                             );
-//                         })}
-//                     </>
-//                 )}
-//             </div>
-//         );
-//     }
-// }
 
 const DisplayInfor = (props) => {
     const { listUsers } = props; //object
     const [toggle, setToggle] = useState(true);
+
+
+    // 
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         document.title = 'test'
+    //     }, 3000)
+    //     console.log('>>> call me useEffect()')
+    // });
+
+    // componentDidMount
+    // useEffect(() => {
+    //     // setTimeout(()=>{
+    //     //     document.title = 'test'
+    //     // },3000)
+    //     console.log('>>> call me useEffect()')
+    // }, []);
+
+    // componentDidUpdate
+    useEffect(() => {
+        // setTimeout(()=>{
+        //     document.title = 'test'
+        // },3000)
+        if (listUsers.length === 0) {
+            alert('Ban da xoa het users')
+        }
+        console.log('>>> call me useEffect()')
+    }, [listUsers]);
+
+    console.log('>>> call me render()')
 
     return (
         <div className="display-infor-container">
