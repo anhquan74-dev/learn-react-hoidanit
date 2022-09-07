75c9ac7254cc7e58db1b1a48861865d624bbea05
anhquan74.dev <anhquan74.dev@gmail.com>
2022-09-07 16:15:01 +0700

    video 33

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,54 +1,81 @@
 import React from "react";
-import './DisplayInfor.scss'
-import logo from '../logo.svg'
+import "./DisplayInfor.scss";
+import logo from "../logo.svg";
 class DisplayInfor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isShowListUsers: true,
+        };
+        console.log('>> 1. call me constructor()')
+    }
 
-    state = {
-        isShowListUsers: true
+    componentDidMount() {
+        console.log('>> call me componentDidMount()')
+        setTimeout(() => { document.title = 'anhquan' }, 3000)
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('>> call me componentDidUpdate()', this.props, prevProps)
+        if (this.props.listUsers !== prevProps.listUsers) {
+            if (this.props.listUsers.length === 5) {
+                alert('You got 5 users')
+            }
+        }
+
     }
 
     handleOnclickShowHideUsers = () => {
         this.setState({
-            isShowListUsers: !this.state.isShowListUsers
-        })
-    }
-
-
+            isShowListUsers: !this.state.isShowListUsers,
+        });
+    };
 
     render() {
-        console.log(this.props)
-        const { listUsers } = this.props
+        console.log('>> call me render()')
+
+        // console.log(this.props);
+        const { listUsers } = this.props;
 
         return (
             <div className="display-infor-container">
-                <img src={logo} alt="logo" /><br />
+                <img src={logo} alt="logo" />
+                <br />
                 List Users:
                 <span onClick={this.handleOnclickShowHideUsers}>
-                    {this.state.isShowListUsers === true ? 'Hide List Users' : 'Show List Users'}
+                    {this.state.isShowListUsers === true
+                        ? "Hide List Users"
+                        : "Show List Users"}
                 </span>
-
-                {this.state.isShowListUsers &&
-                    (<>
-                        {
-                            listUsers.map((user, index) => {
-                                return (
-                                    <>
-                                        <div className="user-item">
-                                            <div key={user.id} className={`user-item-content ${(user.age > 18 ? 'green' : 'red')}`}>
-                                                <h4>My name is {user.name}</h4>
-                                                <h4>I'm {user.age}</h4>
-                                            </div>
-                                            <span className="delete-btn" onClick={() => this.props.handleDeleteUser(user.id)}>X</span>
+                {this.state.isShowListUsers && (
+                    <>
+                        {listUsers.map((user, index) => {
+                            return (
+                                <>
+                                    <div className="user-item">
+                                        <div
+                                            key={user.id}
+                                            className={`user-item-content ${user.age > 18 ? "green" : "red"
+                                                }`}
+                                        >
+                                            <h4>My name is {user.name}</h4>
+                                            <h4>I'm {user.age}</h4>
                                         </div>
-                                        <hr />
-                                    </>
-                                )
-                            })
-                        }
-                    </>)
-                }
+                                        <span
+                                            className="delete-btn"
+                                            onClick={() => this.props.handleDeleteUser(user.id)}
+                                        >
+                                            X
+                                        </span>
+                                    </div>
+                                    <hr />
+                                </>
+                            );
+                        })}
+                    </>
+                )}
             </div>
-        )
+        );
     }
 }
 
