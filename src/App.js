088b0385886d14013fd3ceaf26730ab6a7928e3c
anhquan74.dev <anhquan74.dev@gmail.com>
2022-09-07 21:13:27 +0700

    use bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.scss";
-
+import Header from "./components/Header/Header";
 const App = () => {
 
   return (
     <div className="App">
-      Hello World!
-      <button className='btn btn-primary'>Click</button>
+      <Header />
     </div>
   );
 };
