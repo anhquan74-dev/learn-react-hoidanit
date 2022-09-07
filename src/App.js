82e1a84b19ff3bdcf1e300a830c5fd44b39868c4
anhquan74.dev <anhquan74.dev@gmail.com>
2022-09-07 21:33:45 +0700

    react route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import { Link } from "react-router-dom";
+
 const App = () => {
 
   return (
     <div className="App">
       <Header />
+      <div>
+        Test link
+        <div>
+          <button>
+            <Link to="/users">Go to User Page</Link>
+          </button>
+          <button>
+            <Link to="/admins">Go to Admin Page</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
